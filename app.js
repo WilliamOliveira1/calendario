4c654f49a2413b748a8a31472d8f8da634765701
William Oliveira <1704639@aluno.univesp.br>
2021-02-07 19:04:00 -0300

    set first calendar style

---
+++ app.js
@@ -0,0 +1,37 @@
+var clickDia = 0;
+
+/**
+ * 
+ */
+$(".tableRow").on('click', function(e) {    
+    if(e.target.id < 25 && clickDia < 1) {
+        clickDia += 1;
+        $("#"+e.target.id).css({
+            'border-radius' : '2px',
+            'background-color' : 'red',
+            'box-shadow' : '0 25px #666',
+            'transform' : 'translateY(-25px)'            
+         });    
+    } if(e.target.id <= 31  && clickDia < 1) {
+        clickDia += 1;
+        $("#"+e.target.id).css({
+            'border-radius' : '2px',
+            'background-color' : 'red',
+            'box-shadow' : '0 34px #666',
+            'transform' : 'translateY(-25px)'            
+         }); 
+    }
+});
+
+/**
+ * 
+ */
+$("#limparStyle").on('click', function() {
+    $(".button").css({
+        'border-radius' : '',
+        'background-color' : '',
+        'box-shadow' : '',
+        'transform' : ''            
+     });
+     clickDia = 0;
+});
